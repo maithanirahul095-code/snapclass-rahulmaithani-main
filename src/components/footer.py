c0c8024ddcc43d17d8d70a6856897bb38437ab3f
Rahul Maithani <maithanirahul095@gmail.com>
2026-05-18 18:38:46 +0530

    streamlit setup + homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,10 @@
+import streamlit as st 
+
+def footer_home():
+  
+  st.markdown(f"""
+              
+  <div style = "margin-top:2rem ; display:flex; align-items:center ; justify-content:center ; item-align:center "> 
+   <p style= "font-weight:bold;color:white;"> Created with ❤️ by Rahul Maithani </p>
+  </div>
+              """ , unsafe_allow_html=True)
