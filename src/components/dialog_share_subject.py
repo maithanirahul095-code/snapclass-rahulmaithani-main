db8ce4f4b5616cf3f55e82e4f2ead9cb3d1719a8
Rahul Maithani <maithanirahul095@gmail.com>
2026-05-26 15:23:49 +0530

    Finishing Touches

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -4,7 +4,7 @@ import io
 
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name , subject_code):
-    app_domain = "snapclass-main.streamlit.app"
+    app_domain = "snapclass-rahul-main.streamlit.app"
     join_url = f"{app_domain}/join-code-{subject_code}"
 
     st.header("Scann to Join")
